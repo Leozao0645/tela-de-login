d582400ae9faf7b309c3481e1d8f6962525b1346
Leozao0645 <leonardodesouza0645@gmail.com>
2023-06-26 00:33:36 -0300

    Criando tela de cadastro de usuários

---
+++ cadastro.js
@@ -0,0 +1,25 @@
+function mostrarSenha() {
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if(inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    } else {
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+    }
+}
+
+function mostrarConfirmarSenha() {
+    var inputPass = document.getElementById('confirmar-senha')
+    var btnShowPass = document.getElementById('btn-confirma-senha')
+
+    if(inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    } else {
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+    }
+}
